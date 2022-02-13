2d718f09db46d85fc1ab1b1c8a264b6db3c91564
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-13 20:03:51 +0700

    define business logic

--- src/TodoList.java
+++ src/TodoList.java
@@ -1,5 +1,28 @@
 public class TodoList {
+    public static String[] model = new String[10];
+
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * Display todo list
+     */
+    public static void showTodoList() {
+
+    }
+
+    /**
+     * Add todo list items
+     */
+    public static void addTodoList(){
+
+    }
+
+    /**
+     * Remove todo list items
+     */
+    public static void deleteTodoList(){
 
     }
 }
